09234b2669cab23ee81607903bd366e6c08d7e19
Cruz Ferrer <cruz.ferrer@quantadt.com>
2024-06-16 12:19:42 -0400

    all

---
+++ BlazorAppWebApp/BlazorAppWebApp/Services/INote.cs
@@ -0,0 +1,10 @@
+﻿using BlazorAppWebApp.Model;
+namespace BlazorAppWebApp.Services
+{
+    public interface INote
+    {
+        Task<IList<NoteModel>> GetAllNotes();
+        Task<NoteModel> GetNoteById(int Id);
+        Task<NoteModel> AddNote(NoteModel note);
+    }
+}
